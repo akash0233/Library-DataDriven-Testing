750b5166c4f957c814263db10551e00895c40c1d
akash0233 <akashkale0611@gmail.com>
2025-02-19 15:50:43 +0530

    1234:conflict removed

--- springboot/src/main/java/com/test/mainClass.java
+++ springboot/src/main/java/com/test/mainClass.java
@@ -2,13 +2,7 @@ package com.test;
 
 public class mainClass {
     public static void main(String[] args) {
-        System.out.println("Tushar");
         System.out.println("akash");
-
-
-
-
-
         System.out.println("Kale");
     }
 }
